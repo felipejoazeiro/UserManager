d2d6cc34b264457fdff3b86a21f2afc0b105a292
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-07-19 16:06:18 -0300

    Start of post

--- routes.js
+++ routes.js
@@ -12,5 +12,16 @@ routes.get('/login',(req,res)=>{
 routes.get('/register',(req,res)=>{
     return res.render('register')
 })
+routes.post('/register',(req,res)=>{
+    const keys = Object.keys(req.body)
+    
+    for(key of keys){
+        if(req.body[key] == ""){
+            return res.send('Please, fill all fields')
+        }
+    }
+
+    return res.send(req.body)
+})
 
 module.exports = routes
